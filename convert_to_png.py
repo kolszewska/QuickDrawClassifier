e23bb0ac0c444ef705d969ff7c6e5c93445678e1
kolszewska <k.olszewskaaa@gmail.com>
2018-12-08 22:19:28 +0100

    Reworked convertion script

--- convert_to_png.py
+++ convert_to_png.py
@@ -5,11 +5,21 @@ from PIL import Image, ImageOps
 
 npy_dir = './in/'
 out_dir = './out/'
-
 npy_files = [f for f in os.listdir(npy_dir) if os.path.isfile(os.path.join(npy_dir, f))]
-categories = []
+
+
+def generate_data(number_of_images: range, folder_name: str, index: int):
+    for i in number_of_images:
+        file_name = '{}.png'.format(i + 1)
+        file_path = os.path.join(out_dir, folder_name, categories[index], file_name)
+        img = images[i].reshape(28, 28)
+        f_img = Image.fromarray(img)
+        im = ImageOps.invert(f_img)
+        im.save(file_path, 'png')
+
 
 # Get categories from file names
+categories = []
 for file in npy_files:
     category_split = file.split('_')[3].split('.')[0]
     category = category_split.title()
@@ -29,28 +39,8 @@ for file in npy_files:
     test_images = range(800, 1000, 1)
     validation_images = range(1000, 1150, 1)
 
-    for i in train_images:
-        file_name = '{}.png'.format(i + 1)
-        file_path = os.path.join(out_dir, 'train', categories[category_index], file_name)
-        img = images[i].reshape(28, 28)
-        f_img = Image.fromarray(img)
-        im = ImageOps.invert(f_img)
-        im.save(file_path, 'png')
-
-    for i in test_images:
-        file_name = '{}.png'.format(i + 1)
-        file_path = os.path.join(out_dir, 'test', categories[category_index], file_name)
-        img = images[i].reshape(28, 28)
-        f_img = Image.fromarray(img)
-        im = ImageOps.invert(f_img)
-        im.save(file_path, 'png')
-
-    for i in validation_images:
-        file_name = '{}.png'.format(i + 1)
-        file_path = os.path.join(out_dir, 'validation', categories[category_index], file_name)
-        img = images[i].reshape(28, 28)
-        f_img = Image.fromarray(img)
-        im = ImageOps.invert(f_img)
-        im.save(file_path, 'png')
+    generate_data(train_images, 'train', category_index)
+    generate_data(test_images, 'test', category_index)
+    generate_data(validation_images, 'validation', category_index)
 
     category_index += 1
