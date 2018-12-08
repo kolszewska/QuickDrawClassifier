0fcef5c79eeaad0a7c05be0a636cce2b51c0552f
kolszewska <k.olszewskaaa@gmail.com>
2018-12-08 22:21:59 +0100

    Fixed namings in script

--- convert_to_png.py
+++ convert_to_png.py
@@ -8,7 +8,7 @@ out_dir = './out/'
 npy_files = [f for f in os.listdir(npy_dir) if os.path.isfile(os.path.join(npy_dir, f))]
 
 
-def generate_data(number_of_images: range, folder_name: str, index: int):
+def convert_images(number_of_images: range, images: any, folder_name: str, index: int):
     for i in number_of_images:
         file_name = '{}.png'.format(i + 1)
         file_path = os.path.join(out_dir, folder_name, categories[index], file_name)
@@ -34,13 +34,13 @@ for category in categories:
 # Split pictures to train, test and validation folders
 category_index = 0
 for file in npy_files:
-    images = np.load(os.path.join(npy_dir, file))
+    images_from_npy = np.load(os.path.join(npy_dir, file))
     train_images = range(0, 800, 1)
     test_images = range(800, 1000, 1)
     validation_images = range(1000, 1150, 1)
 
-    generate_data(train_images, 'train', category_index)
-    generate_data(test_images, 'test', category_index)
-    generate_data(validation_images, 'validation', category_index)
+    convert_images(train_images, images_from_npy, 'train', category_index)
+    convert_images(test_images, images_from_npy, 'test', category_index)
+    convert_images(validation_images, images_from_npy, 'validation', category_index)
 
     category_index += 1
