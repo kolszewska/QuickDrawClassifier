54ef23621f32c9b1602559e6ba6895cfa205bf79
kolszewska <k.olszewskaaa@gmail.com>
2018-12-11 19:18:02 +0100

    Added saving model, fixed few bugs, added proper test

--- convert_to_png.py
+++ convert_to_png.py
@@ -28,19 +28,19 @@ for file in npy_files:
 # Create directories
 for category in categories:
     os.makedirs(os.path.join(out_dir, 'train', category))
-    os.makedirs(os.path.join(out_dir, 'test', category))
     os.makedirs(os.path.join(out_dir, 'validation', category))
+    os.makedirs(os.path.join(out_dir, 'test', category))
 
 # Split pictures to train, test and validation folders
 category_index = 0
 for file in npy_files:
     images_from_npy = np.load(os.path.join(npy_dir, file))
     train_images = range(0, 800, 1)
-    test_images = range(800, 1000, 1)
-    validation_images = range(1000, 1150, 1)
+    validation_images = range(800, 1000, 1)
+    test_images = range(1000, 1150, 1)
 
     convert_images(train_images, images_from_npy, 'train', category_index)
-    convert_images(test_images, images_from_npy, 'test', category_index)
     convert_images(validation_images, images_from_npy, 'validation', category_index)
+    convert_images(test_images, images_from_npy, 'test', category_index)
 
     category_index += 1
